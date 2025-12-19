863f7a3676594cb85b33ca0384a948c73b8d2998
Borges <gabrielborgessantos721@gmail.com>
2025-12-19 20:24:06 -0300

    feat: add CORS method and header configurations in settings.py for enhanced API security

--- zapsign_project/settings.py
+++ zapsign_project/settings.py
@@ -156,6 +156,26 @@ CORS_ALLOWED_ORIGINS = config(
 )
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_PREFLIGHT_MAX_AGE = 86400
 
 LOGGING = {
     'version': 1,
